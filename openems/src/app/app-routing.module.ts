0c3f4878a8aa73fe7b84ca4a8a7c468c823f3353
Fabian Schwarzbeck <fabian.schwarzbeck@fenecon.de>
2019-04-01 14:56:57 +0200

    no message

--- openems/src/app/app-routing.module.ts
+++ openems/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'setup', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'overview', loadChildren: './overview/overview.module#OverviewPageModule' },
   { path: 'help-modal', loadChildren: './shared/help-modal/help-modal.module#HelpModalPageModule' },
   { path: 'about-modal', loadChildren: './shared/about-modal/about-modal.module#AboutModalPageModule' },
+  { path: 'setup', loadChildren: './setup/setup.module#SetupPageModule' },
+
 ];
 
 @NgModule({

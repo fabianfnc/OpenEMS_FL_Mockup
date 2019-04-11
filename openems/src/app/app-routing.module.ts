8156fd6b655200e7bf51fe26f29ab5e8df210f48
ffnc <fabian.schwarzbeck@fenecon.de>
2019-04-11 23:51:44 +0200

    no message

--- openems/src/app/app-routing.module.ts
+++ openems/src/app/app-routing.module.ts
@@ -35,12 +35,13 @@ const routes: Routes = [
   { path: 'wechselrichterinfo', loadChildren: './inbetriebnahme/prohybrid/wechselrichter/wechselrichterinfo/wechselrichterinfo.module#WechselrichterinfoPageModule' },
   { path: 'protocolinfo', loadChildren: './inbetriebnahme/prohybrid/protocol/protocolinfo/protocolinfo.module#ProtocolinfoPageModule' },
   { path: 'ibn/commercial/batterie/batterie', loadChildren: './inbetriebnahme/commercial/batterie/batterie.module#BatteriePageModule' },
-  { path: 'ibn/commercial/batterieinfo', loadChildren: './inbetriebnahme/commercial/batterie/batterieinfo/batterieinfo.module#BatterieinfoPageModule' },
+  { path: 'ibn/commercial/batterieinfo', loadChildren: './inbetriebnahme/commercial/batterie/batterieinfo/batterieinfo.module#BatterieinfoPageModuleC' },
   { path: 'ibn/commercial/wechselrichter/wechselrichter', loadChildren: './inbetriebnahme/commercial/wechselrichter/wechselrichter.module#WechselrichterPageModule' },
   { path: 'ibn/commercial/wechselrichter/settings', loadChildren: './inbetriebnahme/commercial/wechselrichter/settings/settings.module#SettingsPageModule' },
-  { path: 'ibn/commercial/wechselrichter/wechselrichterinfo', loadChildren: './inbetriebnahme/commercial/wechselrichter/wechselrichterinfo/wechselrichterinfo.module#WechselrichterinfoPageModule' },
+  { path: 'ibn/commercial/wechselrichter/wechselrichterinfo', loadChildren: './inbetriebnahme/commercial/wechselrichter/wechselrichterinfo/wechselrichterinfo.module#WechselrichterinfoPageModuleC' },
   { path: 'ibn/commercial/wechselrichter/wechselrichtererror', loadChildren: './inbetriebnahme/commercial/wechselrichter/wechselrichtererror/wechselrichtererror.module#WechselrichtererrorPageModule' },
   { path: 'ibn/commercial/batterie/batterieerror', loadChildren: './inbetriebnahme/commercial/batterie/batterieerror/batterieerror.module#BatterieerrorPageModule' },
+
 ]
 
 
